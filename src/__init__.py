286be803bc45deeeeca213825a7a92c23ecff9f3
GuilhermeDornelles <guilherme.dornelles@edu.pucrs.br>
2022-08-24 03:22:20 -0300

    Criando a classe laxycal_analyzer e token

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,26 @@
-# Teste
+class Token:
+    def __init__(self, token, lexema, id):
+        self.token = token
+        self.lexema = lexema
+        self.id = id
+    
+    def print(self):
+        print("<{0}, {1}, {2}>".format(self.token, self.lexema, self.id))
 
-with open('input.txt') as file:
-    lines = file.readlines()
+class Lexycal_analyzer:
+    def __init__(self, count_id=1):
+        self.count_id = count_id
 
-    print(lines)
+    def analyze(self):
+        with open('input.txt') as file:
+            lines = file.readlines()
+        for line in lines:
+            #Está considerando word como cada caractere, deve considerar word como cada palavra
+            for word in line:
+                tok = Token(str(word), word, self.count_id)
+                tok.print()
+                self.count_id = self.count_id + 1
+
+analyzer = Lexycal_analyzer()
+
+analyzer.analyze()
