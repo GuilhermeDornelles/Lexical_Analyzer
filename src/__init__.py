4d12daf8c77c1fc29567483440931e7c95fa69d8
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2022-08-27 18:32:03 -0300

    arrumando o str

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,3 @@
-from ast import match_case
-
 
 class Token:
     def __init__(self, token, lexema, id):
@@ -8,7 +6,8 @@ class Token:
         self.id = id
     
     def __str__(self):
-        return ("<{0}, {1}, {2}>".format(self.token, self.lexema, self.id))
+        return f"('{self.token}', {self.lexema}, {self.id})"
+
 
 class Lexycal_analyzer:
     def __init__(self, count_id=1):
@@ -50,20 +49,25 @@ class Lexycal_analyzer:
                 # self.count_id = self.count_id + 1
     def makeTokens(self, words):
         tokens = []
-        for word, i in enumerate(words):
+        for i, word in enumerate(words):
             if str(word).isdigit():
-                tokens.append(Token(str(word), "int_num", i))
+                token = Token(str(word), "int_num", i)
+                tokens.append(token)
         return tokens
-    
-    #def printTokens(def, tokens)
 
+    def print_tokens(self, tokens):
+        result = "["
+        for token in tokens:
+            result+= token.__str__() + "\n"
+        result+="]"
+        return result
 
 def main():
     analyzer = Lexycal_analyzer()
     words = analyzer.analyze()
     #print(words)
     tokens = analyzer.makeTokens(words)
-    print(tokens)
+    print(analyzer.print_tokens(tokens))
 
 if __name__ == "__main__":
     main()
